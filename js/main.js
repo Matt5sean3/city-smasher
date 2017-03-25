7a6f9eabf2c3e4b71af69c93addfc768f5d9eeb7
Matthew Balch <matbalch@vt.edu>
2017-03-24 23:00:52 -0400

    Changed trigger reset to be a component

--- js/main.js
+++ js/main.js
@@ -4,34 +4,13 @@ require('aframe');
 var physics = require('aframe-physics-system');
 physics.registerAll();
 
-/* I don't like how there's so many global variables here */
-var leftPointing = false;
-var rightPointing = false;
-
 /* Reset by holding the trigger on both controllers */
-var scene = document.querySelector('a-scene');
-
-scene.reload();
-
-var leftHand = document.querySelector('#left-hand[hand-controls]');
-var rightHand = document.querySelector('#right-hand[hand-controls]');
-
-leftHand.addEventListener("pointup", function() {
-    leftPointing = true;
-    if(rightPointing)
-        scene.reload();
-}, false);
-
-leftHand.addEventListener("pointdown", function() {
-    leftPointing = false;
-}, false);
-
-rightHand.addEventListener("pointup", function() {
-    rightPointing = true;
-    if(leftPointing)
-        scene.reload();
-}, false);
-rightHand.addEventListener("pointdown", function() {
-    rightPointing = false;
-}, false);
+AFRAME.registerComponent("trigger-reset", {
+    "init": function() {
+        this.index = triggers.length;
+        this.el.addEventListener("pointup", function() {
+            document.querySelector("a-scene").reload();
+        });
+    }
+});
 
