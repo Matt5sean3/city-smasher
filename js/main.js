9b0c9c86f00218721599bee89b004bdcec7513e2
Matthew Balch <matbalch@vt.edu>
2017-03-24 22:11:06 -0400

    Added reload when both triggers pulled

--- js/main.js
+++ js/main.js
@@ -3,3 +3,35 @@ require('aframe');
 
 var physics = require('aframe-physics-system');
 physics.registerAll();
+
+/* I don't like how there's so many global variables here */
+var leftPointing = false;
+var rightPointing = false;
+
+/* Reset by holding the trigger on both controllers */
+var scene = document.querySelector('a-scene');
+
+scene.reload();
+
+var leftHand = document.querySelector('#left-hand[hand-controls]');
+var rightHand = document.querySelector('#right-hand[hand-controls]');
+
+leftHand.addEventListener("pointup", function() {
+    leftPointing = true;
+}, false);
+
+leftHand.addEventListener("pointdown", function() {
+    leftPointing = false;
+    if(rightPointing)
+        scene.reload();
+}, false);
+
+rightHand.addEventListener("pointup", function() {
+    rightPointing = true;
+}, false);
+rightHand.addEventListener("pointdown", function() {
+    rightPointing = false;
+    if(leftPointing)
+        scene.reload();
+}, false);
+
