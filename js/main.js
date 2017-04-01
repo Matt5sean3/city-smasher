6f940ff6c871f632fa6447edc2be9af325d94ff0
Matthew Balch <matbalch@vt.edu>
2017-04-01 19:35:54 -0400

    Created component for dynamic hand controls

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ AFRAME.registerComponent("destructible", {
         "z_splits": { "default": 3 }
     },
     "init": function() {
+        /* In the ideal case, this would be  */
         /* Make the geometry a static body */
         if(this.data.static)
             this.el.setAttribute("static-body", true);
@@ -22,7 +23,7 @@ AFRAME.registerComponent("destructible", {
             this.el.setAttribute("dynamic-body", true);
 
         if(this.el.components.geometry.data.primitive != "box") {
-            alert("Only boxes can be made destructible");
+            console.warn("Only boxes can be made destructible");
             return;
         }
         this.el.addEventListener("collide", function(e) {
@@ -91,14 +92,122 @@ AFRAME.registerComponent("destructible", {
 });
 
 
-/* Reset by holding the trigger on either controllers */
-AFRAME.registerComponent("trigger-reset", {
+/* Grab a dynamic body using the hand controls */
+AFRAME.registerComponent("grabber", {
+    "schema": {
+        "range": { "default": 20 },
+        "body": { "type": "selector" }
+    },
     "init": function() {
-        this.el.addEventListener("gripclose", function() {
-/*
-            document.querySelector("[structure]").components.structure.reset();
-*/
+        if(!this.data.body)
+            this.data.body = this.el;
+        var body = this.data.body;
+        /* The component also needs raycaster functionality */
+        body.setAttribute("raycaster", {
+            "far": this.data.range,
+            "objects": "[dynamic-body]"
         });
+        this.el.addEventListener("gripclose", this.grab.bind(this));
+        this.el.addEventListener("gripopen", this.release.bind(this));
+    },
+    "grab": function() {
+        var body = this.data.body;
+        var ray = body.raycaster;
+        ray.refreshObjects();
+        /* Nothing to grab */
+        if(ray.intersectedEls.length < 1)
+            return;
+
+        /* Set the constraint attribute */
+        body.setAttribute("constraint", {
+            "type": "lock",
+            "target": ray.intersectedEls[0]
+        });
+    },
+    "release": function() {
+        var body = this.data.body;
+        body.removeAttribute("constraint");
+    }
+});
+
+/* Create hands that respond to dynamic impacts */
+AFRAME.registerComponent("dynamic-hand", {
+    "schema": {
+        "side": { "type": "string", "oneOf": ["left", "right"] }
+    },
+    "init": function() {
+
+        if(!this.el.hasAttribute("dynamic-body"))
+            this.el.setAttribute("dynamic-body", {});
+
+        var staticHand = document.createElement("a-entity");
+        staticHand.setAttribute("visible", false);
+        staticHand.setAttribute("hand-controls", this.data.side);
+        staticHand.setAttribute("static-body", {
+            "shape": "none"
+        });
+        this.el.parentNode.appendChild(staticHand);
+        staticHand.setAttribute("constraint", {
+            "type": "distance",
+            "distance": 0.01,
+            "target": this.el
+        });
+
+        /* Helper entities */
+        this.link = null;
+        this.staticHand = staticHand;
+
+        /* Pass the hand-control events through to this object */
+        var passed = [
+            "gripclose",
+            "gripopen",
+            "pointup",
+            "pointdown",
+            "thumbup",
+            "thumbdown",
+            "pointingstart",
+            "pointingend",
+            "pistolstart",
+            "pistolend"];
+        for(var i = 0; i < passed.length; i++)
+            this.staticHand.addEventListener(passed[i], this.el.emit.bind(this.el, passed[i]));
+    },
+    "remove": function() {
+        /* Remove the static hand */
+        this.staticHand.parentNode.removeElement(this.staticHand);
+    }
+});
+
+AFRAME.registerComponent("city-block", {
+    "schema": {
+        /* Human scale by default */
+        "scale": { "default": 1.0 },
+        /* Mostly just a heuristic of number of average floors per square foot */
+        "density": { "default": 0.5 },
+        /* Ratio of developed square footage to undeveloped */
+        "developed": { "default": 0.85 },
+        /* The type of buildings to generate */
+        "profile": { "default": "business", "oneOf": ["business", "retail", "residential"] }
+    },
+    "init": function() {
+        /* Examines geometry for upward facing facets */
+        if(this.el.components.geometry.primitive !== "plane") {
+            console.warn("Only planes can have city blocks built upon them");
+            return;
+        }
+        if(this.data.developed > 1.0) {
+            console.warn("Cannot have more than 100% of the block built");
+            this.data.developed = 1.0;
+        } else if(this.data.developed < 0.0) {
+            console.warn("Cannot have less than 0% of the block built");
+            this.data.developed = 0.0;
+        }
+        var width = this.el.components.geometry.width / scale,
+            depth = this.el.components.geometry.height / scale,
+            area = width * depth,
+            builtArea = area * this.data.developed,
+            builtFootage = area * this.data.density;
+        
     }
 });
 
